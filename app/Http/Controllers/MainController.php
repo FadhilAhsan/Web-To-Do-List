7b614822465a95d49f5c3756d12f211edc93bb63
Fadhil <fadhilahsan2104@gmail.com>
2018-11-24 06:14:35 +0700

    fixed bug limit frist time load data

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,21 +9,8 @@ class MainController extends Controller
 {
  	public function index()
     {
-    	$limit = 0;
-    	$offset = 0;
-    	if (!request()->has('limit')) {
-			$limit = 10;
-		}else{
-			$limit = request()->input('limit');
-		}
 
-		if (!request()->has('offset')) {
-			$offset = 0;
-		}else{
-			$offset = request()->input('offset');
-		}
-
-        $data = Task::offset($offset)->limit($limit)->get();
+        $data = Task::all();
 
         if ($data == null) {
         	$meta = $this->createMeta(200,true,'success get to do list');
